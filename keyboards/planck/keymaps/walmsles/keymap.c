474a3ade6d52e71d7301a4519ca52bc8f29b0eda
Michael Walmsley <michael.walmsley@digital360.com.au>
2018-01-12 16:55:02 +1100

    Moved AG_SWAP and related keys 2 columns to the left

--- keyboards/planck/keymaps/walmsles/keymap.c
+++ keyboards/planck/keymaps/walmsles/keymap.c
@@ -164,7 +164,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = {
   {_______, RESET,   DEBUG,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_DEL },
-  {_______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, KC_LEFT,  KC_DOWN, KC_UP,  KC_RGHT,  _______},
+  {MU_MOD, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, KC_LEFT,  KC_DOWN, KC_UP,  KC_RGHT,  _______},
   {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______},
   {QWERTY, COLEMAK, DVORAK, PLOVER, _______, KC_MS_BTN1, KC_MS_BTN2, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R}
 }
